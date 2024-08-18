42acc692f9db0bc972c407e950cee6c5cc907f47
Mahak <mahakgarg8002@gmail.com>
2024-08-19 00:49:53 +0530

    updated url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing User Order from Frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:3000";
+  const frontend_url = "https://hungrybox-frontend.onrender.com";
 
   try {
     const newOrder = new orderModel({
--- frontend/src/context/StoreContext.js
+++ frontend/src/context/StoreContext.js
@@ -8,7 +8,7 @@ const StoreContextProvider = (props) => {
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
-  const url = "http://localhost:4000";
+  const url = "https://hungrybox.onrender.com";
 
   const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
