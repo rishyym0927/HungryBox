ffdb8c357134b82e9400bd6a03ccf3a6f61b9634
Mahak <mahakgarg8002@gmail.com>
2024-08-20 20:03:08 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://hungrybox-frontend.onrender.com', // Frontend URL
+}));
 
 // db connection
 connectDB();
