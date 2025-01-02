b5fd63ae24684c676aefd6413cdbf85122515e1a
KajalDeore04 <kajaldeore04@gmail.com>
2025-01-02 15:33:03 +0530

    #10 styled footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -4,41 +4,103 @@ import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div className="footer" id="footer">
-      <div className="footer-content">
-        <div className="footer-left">
-          <img className="logo" src={assets.logo} alt="" />
-          <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="" />
-            <img src={assets.linkedin_icon} alt="" />
-            <img src={assets.twitter_icon} alt="" />
-          </div>
-        </div>
-
-        <div className="footer-center">
-          <h2>Company</h2>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Delivery</li>
-            <li>Privacy</li>
-          </ul>
-        </div>
-
-        <div className="footer-right">
-          <h2>Get in Touch</h2>
-          <ul>
-            <li>+91 0123456789</li>
-            <li>hungerybox@gmail.com</li>
-          </ul>
-        </div>
-      </div>
-
-      <hr />
-      <p className="copyright">
-        Copyright 2024 @ HungryBox.com - All Rights Reserved
-      </p>
+    <section class="section-container">
+    <div class="container">
+        <div class="grid-layout">
+            <div class="brand-container">
+                 <div class="crop">
+        <img src="https://hungrybox-frontend.onrender.com/static/media/logo.7464c701e35f7a0fefb1.png" alt="Donald Duck"/>
     </div>
+
+                <p class="brand-description">
+                Craving something delicious? 🍕 We've got you covered! Fast. Fresh. Reliable. 💨
+                </p>
+
+                <ul class="social-icons">
+                    <li>
+                        <a href="#" title="" class="social-icon twitter">
+                        <img src={assets.twitter_icon} alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="" class="social-icon facebook">
+                        <img src={assets.facebook_icon} alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="" class="social-icon linkedin">
+                        <img src={assets.linkedin_icon} alt="" />
+                        </a>
+                    </li>
+                    
+                </ul>
+            </div>
+
+            <div class="links-container">
+                <p class="section-title">Company</p>
+
+                <ul class="links-list">
+                    <li>
+                        <a href="#" title="" class="link-item">About</a>
+                    </li>
+                    <li>
+                        <a href="#" title="" class="link-item">Features</a>
+                    </li>
+                    <li>
+                        <a href="#" title="" class="link-item">Works</a>
+                    </li>
+                    <li>
+                        <a href="#" title="" class="link-item">Career</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="help-container">
+                <p class="section-title">Help</p>
+
+                <ul class="links-list">
+                    <li>
+                        <a href="#" title="" class="link-item">Customer Support</a>
+                    </li>
+                    <li>
+                        <a href="#" title="" class="link-item">Delivery Details</a>
+                    </li>
+                    <li>
+                        <a href="#" title="" class="link-item">Terms & Conditions</a>
+                    </li>
+                    <li>
+                        <a href="#" title="" class="link-item">Privacy Policy</a>
+                    </li>
+                    
+                </ul>
+            </div>
+            <div class="subscription-container">
+    <p class="subscription-heading">Subscribe to newsletter</p>
+
+    <form action="#" method="POST" class="subscription-form">
+        <div>
+            <label for="email" class="sr-only"></label>
+            <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+                class="email-input"
+            />
+        </div>
+
+        <button type="submit" class="subscribe-button">Subscribe</button>
+    </form>
+</div>
+
+        </div>
+        <hr class="custom-divider" />
+
+<p class="footer-text">© Copyright 2021, All Rights Reserved by hungrybox</p>
+
+    </div>
+</section>
+
   );
 };
 
