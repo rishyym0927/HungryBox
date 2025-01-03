89280fa63d1c113bd634bd64748b56710e01dd55
shagufta28 <saifishagufta82@gmail.com>
2025-01-03 17:11:28 +0530

    UX is enhanced with loading issue

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header-content">
-        <h2>Order your Tiffin now</h2>
-        <p>From our Kitchen to your Doorstep, Taste the Difference!"</p>
+        <h2 className="header-title">Order your Tiffin now</h2>
+        <p className="header-description">From our Kitchen to your Doorstep, Taste the Difference!</p>
       </div>
     </div>
   );
